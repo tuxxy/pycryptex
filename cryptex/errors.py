08b1f31b327f893e8e10a32445e32b3cd296f39f
tuxxy <me@johnpacific.com>
2017-04-15 23:49:28 -0600

    Add errors file

---
+++ cryptex/errors.py
@@ -0,0 +1,22 @@
+class KeysizeError(Exception):
+    """Raised when keysize is invalid for the Cryptex operation.
+
+    Attributes:
+        message -- explanation of error
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+
+class ExpirationError(Exception):
+    """Raised when the token is past the expiration ttl length.
+
+    Attributes:
+        message -- explanation of error
+        expired -- number of seconds since message expired
+    """
+
+    def __init__(self, message, expired):
+        self.message = message
+        self.expired = expired
